be0937ab232be89a87564e7e1ca16303622cb75f
tome <ggsgustavoggs@gmail.com>
2023-10-31 16:47:07 -0300

    cp

---
+++ animals.js
@@ -0,0 +1,6 @@
+const animals = ["macaco", "cachorro", "leão", "gato", "pássaro"]
+if (!animals.includes("leão")){
+  console.log("Leão não existe no array animals")
+}else{
+  console.log("Existe um leão no array animals")
+}
---
+++ series.js
@@ -0,0 +1,7 @@
+const my3FavoriteTVShows= ["Aliced in Borderland", "The Last of Us", "Stranger Things"];
+let check3 = false;
+if (my3FavoriteTVShows.length === 3){
+  check3 = true;
+}
+
+console.log(check3);
